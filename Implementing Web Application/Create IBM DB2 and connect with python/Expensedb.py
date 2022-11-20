ee34a41677a342e28b090ece89c5547769623e2a
vishnu.T <25tvishnu@gmail.com>
2022-11-20 22:54:21 +0530

    completed

---
+++ Implementing Web Application/Create IBM DB2 and connect with python/Expensedb.py
@@ -0,0 +1,4 @@
+import ibm_db
+ibm_db.pconnect("DATABASE=bludb;HOSTNAME=815fa4db-dc03-4c70-869a-a9cc13f33084.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=30367;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=5;PWD=6;",'','')
+print("connection successful...")
+
